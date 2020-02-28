40a4fb647b8c58a08c5ddedbf501e384af753a1a
Firelonze <robinvandenbor@outlook.com>
2020-02-28 11:47:48 +0100

    DONE
    Hp added, Jobs done

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -1,18 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Damage : MonoBehaviour
 {
+    float health;
+    float damage;
+    bool isDead;
+    [SerializeField] Button player;
+    [SerializeField]Text textHp;
     // Start is called before the first frame update
     void Start()
     {
+        health = 100;
+        
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (health <= 0)
+        {
+            health = 0;
+            var colors = GetComponent<Button>().colors;
+            colors.normalColor = Color.gray;
+            GetComponent<Button>().colors = colors;
+            isDead = true;
+            textHp.text = "dead";
+        }
+        else
+        {
+            textHp.text = "HP : " + health.ToString();
+        }
+        damage = Mathf.Round(Random.Range(0f, 10f));
+    }
+    public void TakeDamage()
+    {
+        health -= damage;
     }
 }
